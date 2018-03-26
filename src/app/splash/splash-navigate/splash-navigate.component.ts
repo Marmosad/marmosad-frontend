b0122b504352cf98d1caa56601acce882b23e1e6
Jonathan Yang <jonathan8589@gmail.com>
2018-03-26 05:48:03 -0400

    Slightly changed enter and about button code

--- src/app/splash/splash-navigate/splash-navigate.component.ts
+++ src/app/splash/splash-navigate/splash-navigate.component.ts
@@ -33,6 +33,11 @@ export class SplashNavigateComponent implements OnInit {
     this.animate(this.route);
   }
 
+  // same as toCore() for now
+  toAbout(): void {
+    this.animate(this.route);
+  }
+
   animate = (callRoute): void => {
     console.log('redirecting to core');
     this.routeOut.emit(null);
