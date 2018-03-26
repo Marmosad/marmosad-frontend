afa4356d77a415f7d1da97d52619243c42ceb481
Jonathan Yang <jonathan8589@gmail.com>
2018-03-26 05:35:42 -0400

    Added enter name animation

--- src/app/core/board-core/board-core.component.ts
+++ src/app/core/board-core/board-core.component.ts
@@ -1,24 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketIoService } from '../../socket-io/socket-io.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-board-core',
   templateUrl: './board-core.component.html',
-  styleUrls: ['./board-core.component.css']
+  styleUrls: ['./board-core.component.css'],
+  animations: [
+    trigger('nameAnimationState', [
+      state('start', style({
+        transform: 'translateX(0%) translateY(150%)'
+      })),
+      state('end', style({
+        transform: 'translateX(0%) translateY(-0%)'
+      })),
+      transition('start => end', animate('600ms ease-in'))
+    ])
+  ]
 })
 export class BoardCoreComponent implements OnInit {
   playerName: string;
   hasName: boolean;
+  show = true;
 
   constructor(private socketService: SocketIoService) {
   }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.toggle();
+    }, 1);
   }
 
   setPlayerName = (playerName: string): void => {
     this.playerName = playerName;
     this.hasName = true;
     this.socketService.setPlayerName(playerName);
+  }
+
+  get getState(): String {
+    return this.show ? 'start' : 'end';
+  }
+
+  toggle() {
+    this.show = !this.show;
   }
 }
